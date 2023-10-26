b3fab0310434bb1d886a882d5d20818e5b1d53da
rigolook <rixxormen@gmail.com>
2023-10-26 08:29:05 -0700

    Agregar archivos de proyecto.

---
+++ EjercicioBusquedaBinaria/Ejercicios.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace EjercicioBusquedaBinaria
+    {
+        public class ejercicios { 
+        public static int BusquedaBinariaEnteros(int[] numeros, int numeroBuscado)
+        {
+            int izquierda = 0;
+            int derecha = numeros.Length - 1;
+
+            while (izquierda <= derecha)
+            {
+                int medio = izquierda + (derecha - izquierda) / 2;
+
+                if (numeros[medio] == numeroBuscado)
+                {
+                    return medio;
+                }
+
+                if (numeros[medio] < numeroBuscado)
+                {
+                    izquierda = medio + 1;
+                }
+                else
+                {
+                    derecha = medio - 1;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int BusquedaBinariaCadenas(string[] cadenas, string cadenaBuscada)
+        {
+            int izquierda = 0;
+            int derecha = cadenas.Length - 1;
+
+            while (izquierda <= derecha)
+            {
+                int medio = izquierda + (derecha - izquierda) / 2;
+                int comparacion = string.Compare(cadenas[medio], cadenaBuscada, StringComparison.OrdinalIgnoreCase);
+
+                if (comparacion == 0)
+                {
+                    return medio;
+                }
+
+                if (comparacion < 0)
+                {
+                    izquierda = medio + 1;
+                }
+                else
+                {
+                    derecha = medio - 1;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int BusquedaBinariaNumeroPar(int[] numeros)
+        {
+            int izquierda = 0;
+            int derecha = numeros.Length - 1;
+
+            while (izquierda <= derecha)
+            {
+                int medio = izquierda + (derecha - izquierda) / 2;
+
+                if (numeros[medio] % 2 == 0)
+                {
+                    return medio;
+                }
+
+                if (numeros[medio] < 0)
+                {
+                    izquierda = medio + 1;
+                }
+                else
+                {
+                    derecha = medio - 1;
+                }
+            }
+
+            return -1;
+        }
+            }
+    }
---
+++ EjercicioBusquedaBinaria/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace EjercicioBusquedaBinaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            string[] cadenas = { "apple", "banana", "grape", "orange", "pear" };
+
+            // Búsqueda binaria para números enteros
+            Console.Write("Ingrese un número para buscar en la lista de enteros: ");
+            int numeroBuscado = Convert.ToInt32(Console.ReadLine());
+            int resultadoEnteros = ejercicios.BusquedaBinariaEnteros(numeros, numeroBuscado);
+            if (resultadoEnteros != -1)
+            {
+                Console.WriteLine("El número se encuentra en la posición: " + resultadoEnteros);
+            }
+            else
+            {
+                Console.WriteLine("El número no se encuentra en la lista de enteros.");
+            }
+
+            // Búsqueda binaria para cadenas
+            Console.Write("Ingrese una cadena para buscar en la lista de cadenas: ");
+            string cadenaBuscada = Console.ReadLine();
+            int resultadoCadenas = ejercicios.BusquedaBinariaCadenas(cadenas, cadenaBuscada);
+            if (resultadoCadenas != -1)
+            {
+                Console.WriteLine("La cadena se encuentra en la posición: " + resultadoCadenas);
+            }
+            else
+            {
+                Console.WriteLine("La cadena no se encuentra en la lista de cadenas.");
+            }
+
+            // Búsqueda binaria para encontrar el primer número par
+            int resultadoNumeroPar = ejercicios.BusquedaBinariaNumeroPar(numeros);
+            if (resultadoNumeroPar != -1)
+            {
+                Console.WriteLine("El primer número par se encuentra en la posición: " + resultadoNumeroPar);
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron números pares en la lista de enteros.");
+            }
+
+        }
+    }
+}
